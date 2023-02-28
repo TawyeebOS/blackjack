eb31410f90a462a7f8e73628adb6eeac3656a0fb
TawyeebOS <Tawyeebosoetan@Gmail.com>
2023-02-28 17:30:49 +0000

    Added the Blackjack class, which contains a working game method

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,6 @@
 class Card:
     # class variables
-    ranks_value_dict = {"A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8 ,"9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+    ranks_value_dict = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8 ,"9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
     all_ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
     all_suits = ("Hearts", "Clubs", "Spades", "Diamonds")
 
@@ -17,8 +17,8 @@ class Card:
 
     def getValue(self, handValue = 0): # gets the value of the card from its rank. 
         value = 0
-        if self.rank == "A" and (handValue + 11 <= 21): # special case where Ace can be 11 or 1
-                    value = 11
+        if self.rank == "A" and (handValue + 11 > 21): # special case where Ace can be 11 or 1
+                    value = 1
                     return value
         for rankKey in Card.ranks_value_dict:
             if self.rank == rankKey:
@@ -49,14 +49,22 @@ class Player:
         Net earnings: {net_earnings}
         """.format(name = self.name, wallet = self.wallet, wins = self.wins, loses = self.loses, net_earnings = self.net_earnings)
 
-    def placeBet(self, multiplier): #place bet in table intervals
-        #multiplier = int(input("Enter a multipler for the min bet as a main bet: "))
+    def placeBet(self): #place bet in table intervals
+        multiplier = int(input("Enter a multipler for the min bet (£{minBet}) as your main bet: ".format(minBet = self.dealer.bet)))
         mainBet = self.dealer.bet * multiplier
-        self.wallet -= mainBet
-        self.dealer.mainPot += mainBet 
-        return mainBet
+        if mainBet < self.wallet:
+            self.wallet -= mainBet
+            self.dealer.mainPot += mainBet 
+            return mainBet
+        else:
+            return False
 
-    def placeInsurance(self, insuranceBet): #place an insurance bet, up to half of your main bet if the dealer has an ace upturned incase dealer has blackjack
+    def placeInsurance(self, bet): #place an insurance bet, up to half of your main bet if the dealer has an ace upturned incase dealer has blackjack
+        insuranceBet = 0
+        choice = input("Do you want to place an insurance bet? (y/n): ")
+        if choice.lower() == "y":
+            while insuranceBet <=  0 or insuranceBet > (bet/2):
+                insuranceBet = float(input("Enter an insurance bet up to half (${half}) of your main bet: ".format(half = bet/2)))
         if insuranceBet > self.wallet: #in the case that the insurance is larger than the players wallet 
             self.wallet = 0
         self.wallet -= insuranceBet
@@ -93,6 +101,11 @@ class Player:
 
     def leaveTable(self):
         self.dealer = None
+
+    def displayHand(self):
+        print("Your hand:")
+        for card in self.hand:
+            print(card)
 
     def clearHand(self):
         self.hand = []
@@ -132,12 +145,12 @@ class Dealer:
     def deal(self, player): #take a random card from the deck, give 2 to the player and 2 to yourself
         print("You have", player.hit(), "and", player.hit())
         print("The Dealer has", self.hit())
-        self.hit
+        self.hit()
 
     def checkValue(self, hand): #check the value of a hand
         totalValue = 0
-        for card in hand:
-            totalValue += card.getValue()
+        for i in range(len(hand)):
+            totalValue += hand[i].getValue(totalValue)
         return totalValue
 
     def hit(self): #draw one card from deck
@@ -159,3 +172,137 @@ class Dealer:
     
     def clearHand(self):
         self.hand = []
+
+
+class Blackjack:
+    #static variables
+    dealer_1 = Dealer("Poof", 5)
+    dealer_2 = Dealer("Cosmo", 10)
+    dealer_3 = Dealer("Wanda", 20)
+    dealer_4 = Dealer("Timmy", 50)
+    dealer_5 = Dealer("Vicky", 100)
+    dealers = (dealer_1, dealer_2, dealer_3, dealer_4, dealer_5)
+
+    def __init__(self, player = None):
+        self.player = player
+
+
+    def createPlayer(self):
+        if self.player == None:
+            name = input("What is your name?: ")
+            self.player = Player(name, 500)
+
+    def showDealers(self):
+        n = 1
+        for dealer in Blackjack.dealers:
+            print("Table", n, ":", dealer)
+            n += 1
+
+
+    def assignTable(self):
+        if self.player.dealer == None:
+            self.showDealers()
+            choice = int(input("Which table do you want to play at? (1-5): "))
+            if choice > 0 and choice < len(Blackjack.dealers):
+                self.player.joinTable(Blackjack.dealers[choice-1])
+
+    def game(self):
+        self.createPlayer()
+        self.assignTable()
+
+        while True:
+            bet = self.player.placeBet()
+            if bet == False:
+                print("not enought money")
+                break
+
+            self.player.dealer.deal(self.player)
+            if self.player.dealer.hand[0].rank == "A":
+                self.player.placeInsurance(bet)
+
+            if self.player.dealer.checkValue(self.player.hand) == 21:
+                print("{name} has BlackJack".format(name=self.player.name))
+                if self.player.dealer.checkValue(self.player.dealer.hand) == 21:
+                    print("Dealer has blackjack, unlucky draw")
+                    self.player.dealer.payPlayer(self.player, "insurance")
+                else:
+                    print("Dealer doesn't have blackjack, you win")
+                    self.player.dealer.payPlayer(self.player, "blackjack")
+                if self.player.playAgain():
+                    continue
+                else:
+                    break
+            
+            if self.player.dealer.checkValue(self.player.dealer.hand) == 21:
+                print("The dealers hole card is", self.player.dealer.hand[1])
+                print("Dealer has blackjack")
+                self.player.dealer.payPlayer(self.player, "insurance")
+                if self.player.playAgain():
+                    continue
+                else:
+                    break
+
+            handValue = self.player.dealer.checkValue(self.player.hand)
+            round = 1
+            folding = False
+            while handValue <= 21:
+                if round == 1:
+                    foldChoice = input("Fold? (y/n):")
+                    if foldChoice == "y":
+                        self.player.fold(bet)
+                        folding = True
+                        break
+                choice = input("""
+                1. Hit
+                2. Double Down
+                3. Stand
+
+                Enter choice: """)
+                if choice == "1" or choice.lower() == "hit":
+                    print("You drew", self.player.hit())
+                elif choice == "2" or choice.lower() == "double down":
+                    self.player.doubledown(bet)
+                elif choice == "3" or choice.lower() == "stand":
+                    self.player.stand()
+                    break
+                
+                handValue = self.player.dealer.checkValue(self.player.hand)
+                self.player.displayHand()
+                round += 1
+            if folding:
+                if self.player.playAgain():
+                    continue
+                else:
+                    break
+
+            if handValue > 21:
+                print("Bust. No win")
+                if self.player.playAgain():
+                    continue
+                else:
+                    break
+
+
+            print("The dealers hole card is", self.player.dealer.hand[1])
+            while self.player.dealer.checkValue(self.player.dealer.hand) < 17:
+                print("The dealer drew", self.player.dealer.hit())
+                
+
+            dealerHandValue = self.player.dealer.checkValue(self.player.dealer.hand)
+            print("The dealer hand has a value of", dealerHandValue)
+            print("Your hand has a value of", handValue)
+
+            if dealerHandValue > 21 or dealerHandValue < handValue:
+
+                print("You win")
+                self.player.dealer.payPlayer(self.player, "win")
+            elif dealerHandValue == handValue:
+                print("draw")
+                self.player.dealer.payPlayer(self.player, "draw")
+            else:
+                print("You lose")
+
+            if self.player.playAgain():
+                continue
+            else:
+                break
